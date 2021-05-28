fc0dba947b7961af73d690e841e8cb3c9c21255d
EliezerAmbion <eliezerambion@gmail.com>
2021-05-28 10:13:55 +0800

    finish the categories_controller test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @category = categories(:one)
+  end
+
+  test "should get index" do
+    get categories_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_url
+    assert_response :success
+  end
+
+  test "should create category" do
+    assert_difference 'Category.count', 1 do
+      post categories_url(@category), params: { category: { title: @category.title, description: @category.description } }
+    end
+# home_url is index
+    assert_redirected_to home_url(Category.last)
+  end
+
+  test "should show a category" do
+    get category_url(@category)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_category_url(@category)
+    assert_response :success
+  end
+
+  test "should update category" do
+    put category_url(@category), params: { category: { title: @category.title, description: @category.description } }
+    assert_redirected_to home_url
+  end
+
+  test "should destroy category" do
+    assert_difference('Category.count', -1) do
+      delete category_url(@category)
+    end
+
+    assert_redirected_to home_url
+  end
 end
