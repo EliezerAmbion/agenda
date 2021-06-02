e83add9df67cf00cbcf5c3d867b544ecbcf392a6
EliezerAmbion <eliezerambion@gmail.com>
2021-06-02 19:58:12 +0800

    styling show in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 10 }
 
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
